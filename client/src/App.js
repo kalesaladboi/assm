ecef9cafd7f9745177848ee0336450b434b20b59
kalesaladboi <73973027+kalesaladboi@users.noreply.github.com>
2021-04-13 23:11:52 -0400

    idk something changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 
 function App() {
+
+  const [nameReg, setNameReg] = useState('')
+  const [usernameReg, setUsernameReg] = useState('')
+  const [emailReg, setEmailReg] = useState('')
+  const [birthdayReg, setBirthdayReg] = useState('')
+  const [passwordReg, setPasswordReg] = useState('')  
+  const [password2Reg, setPassword2Reg] = useState('')
+
+
   return (
     <div className="App">
       <h1>Anti Social Social Media</h1>
-      
+        <h2>Register</h2>
+        <label>Enter Name</label>
+        <input type="text"/>
+        <label>Enter Username</label>
+        <input type="text"/>
+        <label>Enter Email</label>
+        <input type="text"/>
+        <label>Enter Birthday</label>
+        <input type="Date"/>
+        <label>Enter Password</label>
+        <input type="password"/>
+        <label>Confirm Password</label>
+        <input type="password2"/>
+        <h2>Login</h2>
+        <label>Enter Email</label>
+        <input type="text"/>
+        <label>Enter Password</label>
+        <input type="password"/>
     </div>
   );
 }
